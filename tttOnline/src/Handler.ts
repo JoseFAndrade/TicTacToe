69adae3269af78e0f2e3db9c6a8d98c3ffbd415e
JoseFAndrade <34077004+JoseFAndrade@users.noreply.github.com>
2026-06-01 14:49:57 -0700

    Started Project
    Started the base of the tic tac toe project. Simple TS to get started and a sample angular page.

---
+++ tttOnline/src/Handler.ts
@@ -0,0 +1,26 @@
+class Handler{
+  private turn: number;
+  private playing: boolean = true;
+  private game: Tictactoe;
+
+  constructor() {
+    this.turn = 0;
+    this.game = new Tictactoe();
+  }
+
+  play(){
+
+    while(this.game.checkPlayable()){
+      //we need to handle now making the turn
+      //turn will always be given over to 0
+
+      this.game.setPiece()
+
+
+    }
+
+    let winner = this.game.checkPlayable();
+
+    return winner;
+  }
+}
---
+++ tttOnline/src/tictactoe.ts
@@ -0,0 +1,83 @@
+class Tictactoe{
+  private grid: number[][];
+  private turn: number; //the number will correspond to the chip as well |an outer function will dictate this
+
+
+  constructor() {
+    this.grid = [[-1,-1,-1],[-1,-1,-1]];
+    /*
+    for(let i = 0; i < 2; i++){
+      for(let y = 0; y < 2; y ++){
+        this.grid[i][y] = -1;
+      }
+    }*/
+    this.turn = 0;
+  }
+
+  setPiece(x: number, y: number, turn: number){
+    this.grid[x][y] = turn;
+  }
+
+  //this function will return whether there is still playable options on the board
+  checkPlayable(): boolean{
+    for(let i = 0; i < 2; i++){
+      for(let y = 0; y < 2; y++){
+        if(this.grid[i][y] === -1)
+          return true;
+      }
+    }
+
+    return false;
+  }
+
+  checkWin(): number{
+    //win condition is 3 in in a row | this can be up/down/diagonal
+
+    //check within a column
+    for(let i = 0; i < 2; i++){
+
+      let continues: boolean = true;
+      let prev = this.grid[i][0];
+      for(let y = 0; y < 2; y++){
+        if(prev === this.grid[i][y]){
+          prev = this.grid[i][y];
+        }
+        else{
+          continues = false;
+          break;
+        }
+      }
+      if(continues)
+        return prev; //returns the number of the winner in this case
+    }
+
+    //check within a row
+    for(let i = 0; i < 2; i++){
+      let continues: boolean = true;
+      let prev = -1;
+      for(let y = 0; y < 2; y++){
+        if(prev === -1 || this.grid[y][i] === prev){
+          prev = this.grid[y][i];
+        }
+        else{
+          continues = false;
+          break;
+        }
+      }
+      if(continues)
+        return prev;
+    }
+
+    //check diagonal -> just going to hard code it for now because its a 3 by 3
+    if(this.grid[0][0] === this.grid[1][1] && this.grid[1][1] === this.grid[2][2])
+      return this.grid[0][0];
+
+    if(this.grid[2][0] === this.grid[1][1] && this.grid[1][1] === this.grid[0][2])
+      return this.grid[2][0];
+
+    //this means that there will be no winner left
+    return -1;
+  }
+
+
+}
