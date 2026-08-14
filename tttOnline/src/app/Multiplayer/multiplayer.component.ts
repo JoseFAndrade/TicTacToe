181c26a1ec60b46f9e8c28b10d2b2ef238d53912
JoseFAndrade <34077004+JoseFAndrade@users.noreply.github.com>
2026-08-14 10:03:08 -0700

    Multiplayer aspect update
    Updated the multiplayer aspect so moves are registered for both players. There is so fixing that is required for testing turns, but other than that its good. Need to check for those things and make sure that I am swapping turns correctly and not letting the UI change when the move is not successfull.

--- tttOnline/src/app/Multiplayer/multiplayer.component.ts
+++ tttOnline/src/app/Multiplayer/multiplayer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Signal, signal } from '@angular/core';
+import { Component, inject, OnInit, Renderer2, Signal, signal } from '@angular/core';
 import { SocketService } from '../../Socket/SocketService';
 
 @Component({
@@ -8,33 +8,56 @@ import { SocketService } from '../../Socket/SocketService';
   styleUrl: './multiplayer.component.css',
 })
 export class Multiplayer {
+  /*
   board: number[][] = [
     [-1, -1, -1],
     [-1, -1, -1],
     [-1, -1, -1],
-  ];
+  ];*/
+
+  board: string[][] = new Array(3).fill(new Array(3).fill("-1"));
 
   room = signal(-1);
   hostWork: boolean = false;
   joinWork: boolean = false;
   displayCreate: boolean = false;
   displayJoin: boolean = false;
-  log = signal("");
+  log = signal('');
   latestMove: boolean = false;
-  message: String = "";
-
+  message: String = '';
 
   socket: SocketService = new SocketService();
+
   x: number = -1;
   y: number = -1;
+  cachedTag: HTMLElement | undefined;
+  private renderer = inject(Renderer2);
+
+  is2DArray<T>(value: any): value is T[][] {
+    return Array.isArray(value) && (value.length === 0 || Array.isArray(value[0]));
+  }
 
   constructor() {
     console.log('testing');
+    for(var i = 0; i < 3; i++){
+      for(var y = 0; y < 3; y++){
+        this.board[i][y] = '-1';
+      }
+    }
+    console.log(this.board);
 
     this.socket.registerRoomStuff.subscribe((data) => {
       console.log('message received?');
       console.log(data);
-      this.log.set(data[3] + "The move made: " + data[1] + " " + data[2]);
+
+      if(this.is2DArray(data[0])){
+        console.log("Wtf");
+        this.board = data[0] as string[][];
+      }
+
+      //this.board = data[0];
+
+      this.log.set(data[3] + 'The move made: ' + data[1] + ' ' + data[2]);
     });
 
     console.log(this.hostWork);
@@ -45,6 +68,7 @@ export class Multiplayer {
     console.log(x + '   ' + y);
     this.x = x;
     this.y = y;
+    this.cachedTag = event.target as HTMLElement;
   }
 
   async create(roomId: String) {
@@ -54,9 +78,8 @@ export class Multiplayer {
     var message = await this.socket.createRoom(id);
     if (message) {
       this.hostWork = true;
-      console.log("what");
+      console.log('what');
       this.room.set(id);
-
     } else {
       this.hostWork = false;
       this.room.set(-1);
@@ -76,13 +99,13 @@ export class Multiplayer {
   }
 
   async makeMove() {
-
-    console.log("what");
-    let list: any[] = [this.socket.getId(),[this.x, this.y]];
+    console.log('what');
+    let list: any[] = [this.socket.getId(), [this.x, this.y]];
+    this.renderer.setStyle(this.cachedTag, 'background-color', 'blue');
     var response = await this.socket.makeMove(this.room(), list);
     if (response) {
-      //alert('The move worked');
-      console.log("move worked");
+      alert('The move worked');
+      console.log('move worked');
     }
   }
 }
